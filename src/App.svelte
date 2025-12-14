d5e86b1e1b08a437a48afa5c1156fc5cd7f5ca43
徐鑫 <2821550908@qq.com>
2025-12-14 23:49:18 +0800

    add: router

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,18 @@
-<div>home</div>
+<script lang="ts">
+  import Router from "svelte-spa-router";
+  import Home from "./routes/Home.svelte";
+  import About from "./routes/About.svelte";
+
+  const routes = {
+    "/": Home,
+    "/about": About,
+  };
+</script>
 
 <svelte:head>
   <title>electron svelte template</title>
 </svelte:head>
+
+<a href="#/">home</a>
+<a href="#/about">about</a>
+<Router {routes}></Router>
